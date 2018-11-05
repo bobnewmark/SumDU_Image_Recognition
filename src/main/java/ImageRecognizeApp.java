b8d07f1e50edab0e7e5a9a3dac4da7d0fce91037
dmsi0417 <bobnewmark@ukr.net>
2018-11-05 14:57:53 +0200

    cleaned, added comments

--- src/main/java/ImageRecognizeApp.java
+++ src/main/java/ImageRecognizeApp.java
@@ -6,24 +6,30 @@ public class ImageRecognizeApp {
 
     public static void main(String[] args) throws IOException {
 
+        // загружаем файлы
         File img1File = new File("src\\main\\resources\\img1.jpg");
         File img2File = new File("src\\main\\resources\\img2.jpg");
         File img3File = new File("src\\main\\resources\\img3.jpg");
         File img4File = new File("src\\main\\resources\\img4.jpg");
 
+        // получаем матрицы яркости изображений (уже в ч/б формате)
         int[][] img1Matrix = Utils.getMatrixFromFile(img1File);
         int[][] img2Matrix = Utils.getMatrixFromFile(img2File);
         int[][] img3Matrix = Utils.getMatrixFromFile(img3File);
         int[][] img4Matrix = Utils.getMatrixFromFile(img4File);
 
+        // создаем вектор усредненных значений
         int[] vector = Calculations.getVectorFromMatrix(img1Matrix);
+        // и вектора верхнего и нижнего допусков
         int[] upperVector = Arrays.stream(vector).map(operand -> operand + 20).toArray();
         int[] lowerVector = Arrays.stream(vector).map(operand -> operand - 20).toArray();
 
+        // получаем бинарные матрицы на основе сравнения с допусками
         int[][] binaryMatrix1 = Calculations.getBinaryMatrix(img1Matrix, upperVector, lowerVector);
         int[][] binaryMatrix2 = Calculations.getBinaryMatrix(img2Matrix, upperVector, lowerVector);
         int[][] binaryMatrix3 = Calculations.getBinaryMatrix(img3Matrix, upperVector, lowerVector);
 
+        // получаем усредненные бинарные вектора
         int[] normalizedBinVector1 = Calculations.getVectorFromMatrix(binaryMatrix1);
         int[] normalizedBinVector2 = Calculations.getVectorFromMatrix(binaryMatrix2);
         int[] normalizedBinVector3 = Calculations.getVectorFromMatrix(binaryMatrix3);
@@ -36,28 +42,8 @@ public class ImageRecognizeApp {
 
 
 
-        //System.out.println("mainMatrix: " + mainMatrix.length + " " + mainMatrix[0].length);
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < img1Matrix.length; i++) {
-            for (int j = 0; j < img1Matrix[i].length; j++) {
-                min = img1Matrix[i][j] < min ? img1Matrix[i][j] : min;
-                max = img1Matrix[i][j] > max ? img1Matrix[i][j] : max;
-            }
-        }
-
 
     }
 
-//    private static BufferedImage fileToImage(File file) throws IOException {
-//        File grey = Grayscale.processFile(file);
-//        return ImageIO.read(grey.toURI().toURL());
-//    }
-//
-//    private static int[][] getMatrixFromFile(File file) throws IOException {
-//        BufferedImage img = fileToImage(file);
-//        return Utils.getBrightnessMatrix(img);
-//    }
+
 }
